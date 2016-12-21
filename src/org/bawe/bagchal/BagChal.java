74a7248f23b0a53c198dea01882eb1c132894463
Stephan Westphal <stephan@wh-ka.de>
2016-12-21 17:26:27 +0100

    initial commit

---
+++ src/org/bawe/bagchal/BagChal.java
@@ -0,0 +1,17 @@
+package org.bawe.bagchal;
+
+/**
+ * Created by stephan on 12/21/16.
+ */
+public class BagChal {
+	private char[][] board;
+
+	public void move(int x, int y){
+		// check move, if it is allowed
+	}
+
+	public char[][] getBoard(){
+
+		return this.board;
+	}
+}
