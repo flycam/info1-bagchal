af9e9eca5e6fd86695f36ab9f01f7ae223a52afd
stephan <stephan@wh-ka.de>
2017-01-20 07:51:34 +0100

    updated tests

--- src/org/bawe/bagchal/BagChalTest.java
+++ src/org/bawe/bagchal/BagChalTest.java
@@ -11,73 +11,7 @@ public class BagChalTest extends TestCase{
         assertTrue(game.getNumGoatsEaten() == 0);
     }
 
-    @org.junit.Test
-    public void getNumGoatsAvailable() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void getCurrentPlayer() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void getWinner() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void countGoatsEaten() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void countPossibleTigerMoves() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void checkBounds() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void checkMoveValidity() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void getOccupancyType() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void checkOccupancy() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void countPossibleMoves() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void setGoat() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void moveGoat() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void moveTiger() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void getBoard() throws Exception {
+    public void testGetBoard() {
 
     }
 
